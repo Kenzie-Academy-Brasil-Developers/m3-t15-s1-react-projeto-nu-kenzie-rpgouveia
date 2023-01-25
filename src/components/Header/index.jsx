4970deaa1efecae27cd3f2fdd10e7435e33fb5a7
Renato Gouveia <rpgouveia@gmail.com>
2023-01-25 12:05:43 -0300

    feat: add header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+function Header() {
+    return (
+        <header className='header'>
+            <h1><span>Nu</span>Kenzie</h1>
+            <button type='submit'>Início</button>
+        </header>
+    )
+}
+
+export default Header
