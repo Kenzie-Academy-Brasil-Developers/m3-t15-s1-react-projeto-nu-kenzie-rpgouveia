05507d9cb6044c8bc8d6e2176b5976b6c43534ca
Renato Gouveia <rpgouveia@gmail.com>
2023-01-28 14:41:08 -0300

    feat: add state functionality for formData

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,27 +1,46 @@
+import { useState } from 'react'
 import styles from './style.module.css'
 
 export const Form = () => {
+
+    const [formData, setFormData] = useState({
+        description: '',
+        value: '',
+        type: 'entrada'
+    })
+
+    function handleSubmit(event) {
+        event.preventDefault()
+    }
+
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
             <div className={styles.form_input_1}>
-                <label className={styles.form_input_label}>Descrição</label>
+                <label className={styles.form_input_label} htmlFor='description'>Descrição</label>
                 <input
+                    id='description'
                     type={'text'}
                     placeholder={'Digite aqui sua descrição'}
+                    onChange={(event) => setFormData({...formData, description: event.target.value})}
                 />
                 <p>Ex: Compra de Roupas</p>
             </div>
             <div className={styles.form_input_2}>
                 <div className={styles.form_input_left}>
-                    <label className={styles.form_input_label}>Valor</label>
+                    <label className={styles.form_input_label} htmlFor='value'>Valor</label>
                     <input
+                        id='value'
                         type={'number'}
+                        onChange={(event) => setFormData({...formData, value: event.target.value})}
                     />
                     <p>R$</p>
                 </div>
                 <div className={styles.form_input_right}>
-                    <label className={styles.form_input_label}>Tipo de Valor</label>
-                    <select>
+                    <label className={styles.form_input_label} htmlFor='type'>Tipo de Valor</label>
+                    <select
+                        id='type'
+                        onChange={(event) => setFormData({...formData, type: event.target.value})}
+                    >
                         <option value={'entrada'}>Entrada</option>
                         <option value={'despesa'}>Despesa</option>
                     </select>
