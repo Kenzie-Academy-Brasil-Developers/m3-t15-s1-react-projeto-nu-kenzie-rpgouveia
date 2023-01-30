ea45fa00bee3cc815f946d1a12952003af2139f1
Renato Gouveia <rpgouveia@gmail.com>
2023-01-29 21:29:49 -0300

    fix: update header img link for deploy in vercel

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,12 @@
 import styles from './style.module.css'
+import NuKenzie from '/src/assets/NuKenzie.svg'
 
 function Header({setCurrentPage}) {
     return (
         <header className={styles.header}>
             <div className={styles.container}>
                 <div className={styles.header_menu}>
-                    <img className={styles.header_img} src='./src/assets/NuKenzie.svg' alt='NuKenzie Logo' />
+                    <img className={styles.header_img} src={NuKenzie} alt='NuKenzie Logo' />
                     <button className={styles.header_btn} type='submit' onClick={() => setCurrentPage('')}>Início</button>
                 </div>
             </div>
